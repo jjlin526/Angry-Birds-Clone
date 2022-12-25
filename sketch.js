7651d64e1fc18da4d02dfa2d9c79cbd2854e76e4
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-25 14:12:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -238,7 +238,7 @@ function drawGameInstructions() {
 
   // Player control keyboard instructions
   text("Drag the octagon to fire it using the slingshot", 245, 115);
-  text("Press 'r' to reset the ball to its initial position", 245, 140);
+  text("Press 'r' to reset the octagon to its initial position", 245, 140);
   text("Press 'b' to spawn a shape at your mouse position", 245, 165);
   text("Press 'left arrow' to spin propeller clockwise", 245, 190);
   text("Press 'right arrow' to spin propeller counterclockwise", 245, 215);
